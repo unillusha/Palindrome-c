67f0b00942f9925b0006ff2ff092f762099c03c3
unillusha <112888979+unillusha@users.noreply.github.com>
2024-09-06 17:29:40 +0100

    Add palindrome check function

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int isPalindrome(const char *s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return 0;
+        }
+        left++;
+        right--;
+    }
+    return 1;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    char input[100];
+    printf("Enter the string: ");
+    scanf("%s", input);
+
+    if (isPalindrome(input)) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
     return 0;
 }
